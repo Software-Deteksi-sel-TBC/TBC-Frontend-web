2016f49d5ceeaad99e4bd41c1f7ed69654c84c32
Cokorda Gede Satria Widnyana Putra <coksatria2005@gmail.com>
2026-02-18 16:45:48 +0700

    Initial React + TS + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind v4 Works 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
